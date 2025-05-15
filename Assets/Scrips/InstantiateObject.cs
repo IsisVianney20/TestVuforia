632309f08349a2d40e45b16e69c9c1a1cd8c2057
IsisVianney20 <mviann479@gmail.com>
2025-05-14 20:43:00 -0600

    Implementar boton, baile y música

---
+++ Assets/Scrips/InstantiateObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToInstantiate;
+    
+    public void Instantiate(Transform target)
+    {
+        Instantiate(objectToInstantiate, target.position, Quaternion.identity);
+    }
+}
