c40a4eae85a23a12e15b71a4e8cdd36dc4011c2a
IsisVianney20 <mviann479@gmail.com>
2025-06-04 21:17:38 -0600

    Fondo y botones

--- Assets/Scrips/NotesChecker.cs
+++ Assets/Scrips/NotesChecker.cs
@@ -6,16 +6,11 @@ public class NotesChecker : MonoBehaviour
 {
     [SerializeField]
     private UnityEvent onButtonPressed;
-
     [SerializeField]
-
     private UnityEvent onCorrectNote;
-
     [SerializeField]
-
     private UnityEvent onFailNote;
     private List<GameObject> notes = new List<GameObject>();
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Note"))
@@ -23,7 +18,6 @@ public class NotesChecker : MonoBehaviour
             notes.Add(collision.gameObject);
         }
     }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Note"))
@@ -31,28 +25,31 @@ public class NotesChecker : MonoBehaviour
             notes.Remove(collision.gameObject);
         }
     }
-
     public void DestroyNotes()
     {
-        if (notes.Count > 0)
+        onButtonPressed?.Invoke();
+        GameObject noteToDestroy = null;
+        int indexToRemove = -1;
+        for (int i = 0; i < notes.Count; i++)
+        {
+            if (notes[i] != null)
+            {
+                noteToDestroy = notes[i];
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        if (noteToDestroy != null)
         {
             onCorrectNote?.Invoke();
+            notes.RemoveAt(indexToRemove);
+            Destroy(noteToDestroy);
         }
         else
         {
             onFailNote?.Invoke();
         }
-        onButtonPressed?.Invoke();
-        while (notes.Count > 0)
-        {
-            GameObject note = notes[0];
-            notes.RemoveAt(0);
-            Destroy(note);
-        }
-        notes.Clear();
     }
 
-
 }
-
-    
